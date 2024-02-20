ce565875623afe4637ee55e0f0a5e13eaf143136
Magomedali Gajiev <mag198421@gmail.com>
2024-02-21 00:11:47 +0300

    Add project files.

---
+++ Server/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+using System.Linq;
+
+
+
+ServerObject server = new ServerObject();// создаем сервер
+await server.ListenAsync(); // запускаем сервер
+
+class ServerObject
+{
+    TcpListener tcpListener = new TcpListener(IPAddress.Any, 8888);
+    List<ClientObject> clients = new List<ClientObject>();
+
+    protected internal void RemoveConnection(string id)
+    {
+        ClientObject client = clients.FirstOrDefault(c => c.Id == id);
+        if (client != null)
+        {
+            clients.Remove(client);
+            client.Close();
+        }
+    }
+
+    protected internal async Task ListenAsync()
+    {
+        try
+        {
+            tcpListener.Start();
+            Console.WriteLine("Сервер запущен. Ожидание подключений...");
+            while (true)
+            {
+                TcpClient tcpClient = await tcpListener.AcceptTcpClientAsync();
+                ClientObject clientObject = new ClientObject(tcpClient, this);
+                clients.Add(clientObject);
+                Task.Run(clientObject.ProcessAsync);
+            }
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+        }
+        finally
+        {
+            Disconnect();
+        }
+    }
+
+    protected internal async Task BroadcastMessageAsync(string message, string id)
+    {
+        foreach (var client in clients)
+        {
+            if (client.Id != id)
+            {
+                await client.Writer.WriteLineAsync(message);
+                await client.Writer.FlushAsync();
+            }
+        }
+    }
+
+    protected internal void Disconnect()
+    {
+        foreach (var client in clients)
+        {
+            client.Close();
+        }
+        tcpListener.Stop();
+    }
+}
+
+class ClientObject
+{
+    protected internal string Id { get; } = Guid.NewGuid().ToString();
+    protected internal StreamWriter Writer { get; }
+    protected internal StreamReader Reader { get; }
+    TcpClient client;
+    ServerObject server;
+
+    public ClientObject(TcpClient tcpClient, ServerObject serverObject)
+    {
+        client = tcpClient;
+        server = serverObject;
+        var stream = client.GetStream();
+        Reader = new StreamReader(stream);
+        Writer = new StreamWriter(stream);
+    }
+
+    public async Task ProcessAsync()
+    {
+        try
+        {
+            string userName = await Reader.ReadLineAsync();
+            string message = $"{userName} вошел в чат";
+            await server.BroadcastMessageAsync(message, Id);
+            Console.WriteLine(message);
+
+            while (true)
+            {
+                string receivedMessage = await Reader.ReadLineAsync();
+                if (receivedMessage == null) continue;
+
+                if (receivedMessage.ToLower() == "exit")
+                {
+                    message = $"{userName} покидает чат";
+                    Console.WriteLine(message);
+                    await server.BroadcastMessageAsync(message, Id);
+                    break;
+                }
+
+                message = $"{userName}: {receivedMessage}";
+                Console.WriteLine(message);
+                await server.BroadcastMessageAsync(message, Id);
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            server.RemoveConnection(Id);
+        }
+    }
+
+    protected internal void Close()
+    {
+        Writer.Close();
+        Reader.Close();
+        client.Close();
+    }
+}
